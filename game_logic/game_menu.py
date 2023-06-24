83081bc2f5b11da519a58ed5ae3185ecbc7aacbe
Ahmad-Alanati <ahmadjehadalanati1@gmail.com>
2023-06-24 12:47:37 +0300

    making the menu as OOP with a new image forthe menu assets

--- game_logic/game_menu.py
+++ game_logic/game_menu.py
@@ -2,28 +2,112 @@ import pygame
 
 pygame.init()
 
-class Menus:
-    def __init__(self):
+
+# start menu class
+class StartGameMenu:
+    def __init__(self,screen_width,screen_height):
           self.font = pygame.font.Font("assets\\fonts\game_over.ttf",128)
+          self.play_button = Button("Play",(screen_width*0.5, screen_height*0.45),)
+          self.quit_button = Button("Quit",(screen_width * 0.5, screen_height * 0.7))
+          self.screen_width = screen_width
+          self.screen_height = screen_height
 
     def logo_info(self,position):
         logo = self.font.render("Pixel Rampage",False,"#F5F5F5")
         logo_rect = logo.get_rect(midtop = position)
         return logo,logo_rect
-
-    def button(self, position, img, button_text,text_color):
-        text = self.font.render(button_text,True,text_color)
-        button_image = pygame.image.load(img)
-        button_image_rect = button_image.get_rect(center=position)
-        text1_pos =(position[0], position[1]-(position[1]*0.02))
-        text_rect = text.get_rect(center = text1_pos)
-        return button_image,button_image_rect,text,text_rect
     
-    def start_game_menu(self,buttons_path,screen_width,screen_height,text_color):
-        logo,logo_rect = self.logo_info((screen_width*0.5, screen_height*0.15))
-        button_play_image,button_play_image_rect,play_game_text,play_game_text_rect = self.button((screen_width*0.5, screen_height*0.45),buttons_path[0],"Play",text_color[0])
-        button_quit_image,button_quit_image_rect,quit_text,quit_text_rect = self.button((screen_width * 0.5, screen_height * 0.7),buttons_path[1],"Quit",text_color[1])
-        return logo, logo_rect, button_play_image, button_play_image_rect, play_game_text, play_game_text_rect, button_quit_image, button_quit_image_rect, quit_text, quit_text_rect   
+    def start_game_draw(self,screen):
+        logo,logo_rect = self.logo_info((self.screen_width*0.5, self.screen_height*0.15))
+        screen.blit(logo,logo_rect)
+        self.play_button.draw(screen)
+        self.quit_button.draw(screen)
+
+# start menu class
+class Button:
+    def __init__(self,button_text, position):
+        self.font = pygame.font.Font("assets\\fonts\game_over.ttf",128)
+        self.button_text = button_text
+        self.button_image = pygame.image.load("assets\\menu_assets\\button_off.png")
+        self.text = self.font.render(button_text,True,"#F5F5F5")
+        self.button_rect = self.button_image.get_rect(center=position)
+        text1_pos =(position[0], position[1]-(position[1]*0.02))
+        self.text_rect = self.text.get_rect(center = text1_pos)
+
+    def hover(self):
+        if self.button_rect.collidepoint(pygame.mouse.get_pos()):
+            self.button_image = pygame.image.load("assets\\menu_assets\\button_on.png") 
+            self.text = self.font.render(self.button_text,False,"#CD7F32")
+        else:
+            self.button_image = pygame.image.load("assets\\menu_assets\\button_off.png")
+            self.text = self.font.render(self.button_text,False,"#F5F5F5")
+    
+    def button_clicked(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.button_rect.collidepoint(event.pos) and event.button == 1:
+                return True
+            else:
+                return False
+
+    def draw(self,screen):
+        screen.blit(self.button_image,self.button_rect)
+        screen.blit(self.text,self.text_rect)
+
+# write puase menu here
+
+
+
+
+
+
+
+#############################################################
+
+def quit_game():
+    pygame.quit()
+    exit()
         
-    def pause_game_menu(self,screen_width,screen_height,text_color):
-        pass  
+# this is the main function
+def main():
+    pygame.display.set_caption("Pixel Rampage")
+    screen_width = 1280
+    screen_height = 720
+    screen = pygame.display.set_mode((screen_width,screen_height))
+    start_game_menu = StartGameMenu(screen_width, screen_height)
+    game_state = "start_game"
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quit_game()
+            if game_state == "start_game":
+                start_game_menu.play_button.hover()
+                start_game_menu.quit_button.hover()
+                if start_game_menu.play_button.button_clicked(event):
+                    game_state = "playing"
+                if start_game_menu.quit_button.button_clicked(event):
+                    quit_game()
+            elif game_state == "playing":
+                #puase menu events handler
+                pass
+
+
+            
+                #######################################################
+
+                
+        if game_state == "start_game":
+            start_game_menu.start_game_draw(screen)
+        elif game_state == "playing":
+            screen.fill((0, 0, 0)) #this line here is to fill the screen with black
+            # puase menu draw
+
+
+
+            ######################################################
+
+
+        pygame.display.update()
+
+if __name__ == "__main__":
+    main()
