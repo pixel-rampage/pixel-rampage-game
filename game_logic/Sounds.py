8d9eadba768ae453c70456755bb9201abe7f43c8
Duaa <drdoaamelhem@gmail.com>
2023-06-26 12:14:02 +0300

    modify

--- game_logic/Sounds.py
+++ game_logic/Sounds.py
@@ -1,101 +1,101 @@
 import pygame
+import os
 import pygame.mixer
+
 pygame.init()
 pygame.mixer.init()
 
-class Sounds:
+class Sound:
     def __init__(self):
-        self.Background_Level1=pygame.mixer.Sound("assets\audio\b_Level1.ogg")
-        self.Background_Level2=pygame.mixer.Sound("assets\audio\bg_L2.mp3")
-        self.win=pygame.mixer.Sound("assets\audio\win_sound.mp3")
-        self.loss=pygame.mixer.Sound("assets\audio\loss_sound.wav")
-        self.End_Level=pygame.mixer.Sound("assets\audio\endoflevels.mp3")
-        self.jump=pygame.mixer.Sound("assets\audio\Jump.mp3")
-        self.Key=pygame.mixer.Sound("assets\audio\Key.wav")
-        self.attack=pygame.mixer.Sound("assets\audio\attack.mp3")
-        self.coins=pygame.mixer.Sound("assets\audio\coins.wav")
-        self.select=pygame.mixer.Sound("assets\audio\select_sound.mp3")
-        self.resume=pygame.mixer.Sound("assets\audio\Waiting III Looped.ogg")
+        self.Background_Level1 = os.path.join("assets","audio", "b_Level1.ogg")
+        self.Background_Level2 = os.path.join("assets","audio", "bg_L2.mp3")
+        self.win = os.path.join("assets","audio", "win_sound.mp3")
+        self.loss = os.path.join("assets","audio", "loss_sound.wav")
+        self.End_Level = os.path.join("assets","audio", "endoflevels.mp3")
+        self.jump = os.path.join("assets","audio", "Jump.mp3")
+        self.Key = os.path.join("assets","audio", "Key.wav")
+        self.attack =os.path.join ("assets","audio", "attack.mp3")
+        self.coins = os.path.join("assets","audio", "coins.mp3")
+        self.select = os.path.join("assets","audio", "select_sound.mp3")
+        self.resume = os.path.join("assets","audio", "Waiting III Looped.ogg")
+#assets\audio\attack.mp3
+    def play_background_level1(self):
+        pygame.mixer.music.load(self.Background_Level1)
+        pygame.mixer.music.play()
 
-    def set_volume(self, volume):
-        pygame.mixer.music.set_volume(volume)
-        for sound in self.__dict__.values():
-            if isinstance(sound, pygame.mixer.Sound):
-                sound.set_volume(volume)
+    def play_background_level2(self):
+        pygame.mixer.music.load(self.Background_Level2)
+        pygame.mixer.music.play()
 
-    def play_background_level1(self, volume=1.0):
-        self.Background_Level1.set_volume(volume)
-        self.Background_Level1.play()
+    def play_win(self):
+        pygame.mixer.music.load(self.win)
+        pygame.mixer.music.play()
 
-    def play_background_level2(self, volume=1.0):
-        self.Background_Level2.set_volume(volume)
-        self.Background_Level2.play()
+    def play_loss(self):
+        pygame.mixer.music.load(self.loss)
+        pygame.mixer.music.play()
 
-    def play_win(self, volume=1.0):
-        self.win.set_volume(volume)
-        self.win.play()
+    def play_end_level(self):
+        pygame.mixer.music.load(self.End_Level)
+        pygame.mixer.music.play()
 
-    def play_loss(self, volume=1.0):
-        self.loss.set_volume(volume)
-        self.loss.play()
+    def play_jump(self):
+        pygame.mixer.music.load(self.jump)
+        pygame.mixer.music.play()
 
-    def play_end_level(self, volume=1.0):
-        self.End_Level.set_volume(volume)
-        self.End_Level.play()
+    def play_key(self):
+        pygame.mixer.music.load(self.Key)
+        pygame.mixer.music.play()
 
-    def play_jump(self, volume=1.0):
-        self.jump.set_volume(volume)
-        self.jump.play()
+    def play_attack(self):
+        pygame.mixer.music.load(self.attack)
+        pygame.mixer.music.play()
 
-    def play_key(self, volume=1.0):
-        self.Key.set_volume(volume)
-        self.Key.play()
+    def play_coins(self):
+        pygame.mixer.music.load(self.coins)
+        pygame.mixer.music.play()
 
-    def play_attack(self, volume=1.0):
-        self.attack.set_volume(volume)
-        self.attack.play()
+    def play_select(self):
+        pygame.mixer.music.load(self.select)
+        pygame.mixer.music.play()
 
-    def play_coins(self, volume=1.0):
-        self.coins.set_volume(volume)
-        self.coins.play()
-
-    def play_select(self, volume=1.0):
-        self.select.set_volume(volume)
-        self.select.play()
-
-    def play_resume(self, volume=1.0):
-        self.resume.set_volume(volume)
-        self.resume.play()
+    def play_resume(self):
+        pygame.mixer.music.load(self.resume)
+        pygame.mixer.music.play()
 
     def stop_background_level1(self):
-        self.Background_Level1.stop()
+        pygame.mixer.music.stop()
 
     def stop_background_level2(self):
-        self.Background_Level2.stop()
+        pygame.mixer.music.stop()
 
     def stop_win(self):
-        self.win.stop()
+        pygame.mixer.music.stop()
 
     def stop_loss(self):
-        self.loss.stop()
+        pygame.mixer.music.stop()
 
     def stop_end_level(self):
-        self.End_Level.stop()
+        pygame.mixer.music.stop()
 
     def stop_jump(self):
-        self.jump.stop()
+        pygame.mixer.music.stop()
 
     def stop_key(self):
-        self.Key.stop()
+        pygame.mixer.music.stop()
 
     def stop_attack(self):
-        self.attack.stop()
+        pygame.mixer.music.stop()
 
     def stop_coins(self):
-        self.coins.stop()
+        pygame.mixer.music.stop()
 
     def stop_select(self):
-        self.select.stop()
+        pygame.mixer.music.stop()
 
     def stop_resume(self):
-        self.resume.stop()
+        pygame.mixer.music.stop()
+if __name__== "__main__":
+    sound0 = Sound()
+    sound0.play_loss()
+    print("Audio")
