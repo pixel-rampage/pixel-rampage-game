9e47a3b5f40b08d27d72b56070dbe561e9092577
Duaa <drdoaamelhem@gmail.com>
2023-06-26 14:00:31 +0300

    sounds- playing

--- game_logic/Sounds.py
+++ game_logic/Sounds.py
@@ -7,18 +7,19 @@ pygame.mixer.init()
 
 class Sound:
     def __init__(self):
-        self.Background_Level1 = os.path.join("assets","audio", "b_Level1.ogg")
-        self.Background_Level2 = os.path.join("assets","audio", "bg_L2.mp3")
-        self.win = os.path.join("assets","audio", "win_sound.mp3")
-        self.loss = os.path.join("assets","audio", "loss_sound.wav")
-        self.End_Level = os.path.join("assets","audio", "endoflevels.mp3")
-        self.jump = os.path.join("assets","audio", "Jump.mp3")
-        self.Key = os.path.join("assets","audio", "Key.wav")
-        self.attack =os.path.join ("assets","audio", "attack.mp3")
-        self.coins = os.path.join("assets","audio", "coins.mp3")
-        self.select = os.path.join("assets","audio", "select_sound.mp3")
-        self.resume = os.path.join("assets","audio", "Waiting III Looped.ogg")
+        self.Background_Level1 = pygame.mixer.Sound(r"C:\Users\doaaz\projects\pixel-rampage-game\game_logic\assets\audio\b_Level1.ogg")
+        self.Background_Level2 = pygame.mixer.Sound(r"C:\Users\doaaz\projects\pixel-rampage-game\game_logic\assets\audio\bg_L2.mp3")
+        self.win = pygame.mixer.Sound(r"C:\Users\doaaz\projects\pixel-rampage-game\game_logic\assets\audio\win_sound.mp3")
+        self.loss = pygame.mixer.Sound(r"C:\Users\doaaz\projects\pixel-rampage-game\game_logic\assets\audio\loss_sound.wav")
+        self.End_Level = pygame.mixer.Sound(r"C:\Users\doaaz\projects\pixel-rampage-game\game_logic\assets\audio\endoflevels.mp3")
+        self.jump = pygame.mixer.Sound("assets\audio\Jump.mp3")
+        self.Key = pygame.mixer.Sound(r"C:\Users\doaaz\projects\pixel-rampage-game\game_logic\assets\audio\Key.wav")
+        self.attack =pygame.mixer.Sound (r"C:\Users\doaaz\projects\pixel-rampage-game\game_logic\assets\audio\attack.mp3")
+        self.coins = pygame.mixer.Sound(r"C:\Users\doaaz\projects\pixel-rampage-game\game_logic\assets\audio\coins.mp3")
+        self.select = pygame.mixer.Sound(r"C:\Users\doaaz\projects\pixel-rampage-game\game_logic\assets\audio\select.wav")
+        self.resume = pygame.mixer.Sound(r"C:\Users\doaaz\projects\pixel-rampage-game\game_logic\assets\audio\resume.ogg")
 #assets\audio\attack.mp3
+
     def play_background_level1(self):
         pygame.mixer.music.load(self.Background_Level1)
         pygame.mixer.music.play()
@@ -40,8 +41,8 @@ class Sound:
         pygame.mixer.music.play()
 
     def play_jump(self):
-        pygame.mixer.music.load(self.jump)
-        pygame.mixer.music.play()
+        c=pygame.mixer.music.load(self.jump)
+        pygame.mixer.music.play(c)
 
     def play_key(self):
         pygame.mixer.music.load(self.Key)
@@ -97,5 +98,6 @@ class Sound:
         pygame.mixer.music.stop()
 if __name__== "__main__":
     sound0 = Sound()
-    sound0.play_loss()
+    sound0.play_jump()
     print("Audio")
+  
