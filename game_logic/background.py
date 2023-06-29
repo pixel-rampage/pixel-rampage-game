a498680a12e1cb23e3b67026901e32d44c9ee89a
Moath-shakhatreh <moath.shakhatreh@gmail.com>
2023-06-29 14:01:57 +0300

    I edited the monster class, so now i can add more monsters in any place in the map snd in any speed i want

--- game_logic/background.py
+++ game_logic/background.py
@@ -15,8 +15,7 @@ pygame.display.set_caption("assets\Parallax")
 
 #define game variables
 scroll = 0
-scroll_S_L = False
-scroll_S_R = False
+
 
 # images
 ground_image = pygame.image.load("assets\Parallax\\aliens_ground_04-modified.png").convert_alpha()
@@ -41,23 +40,13 @@ def draw_bg():
       screen.blit(i, ((x * bg_width) - scroll * speed, 0))
       speed += 0.1
 
-# def update_bg(scroll):
-	
-# 	#get keypresses
-# 	key = pygame.key.get_pressed()
-# 	if key[pygame.K_LEFT] and player.sprite.rect.left < 201 and scroll > 0:
-# 		scroll += 5
-		
-# 	if key[pygame.K_RIGHT] and player.sprite.rect.right > 999 and scroll < 3000:
-# 		scroll -= 5
-		
- 
+
 
 def image(direction):
 	if direction == "left":
 		image = pygame.image.load("assets\character animation\idel\idel1.png").convert_alpha()
 		image = pygame.transform.scale2x(image)
-		image = pygame.transform.flip(image,True,False) 
+		image = pygame.transform.flip(image,True,False)
 	elif direction == "right":
 		image = pygame.image.load("assets\character animation\idel\idel1.png").convert_alpha()
 		image = pygame.transform.scale2x(image)
@@ -75,12 +64,17 @@ def image(direction):
 	elif direction == "big_land" :
 		image = pygame.image.load("assets\Parallax\\aliens_big_ground_7-modified.png").convert_alpha()
 	elif direction == "eater":
-		image = [pygame.image.load("assets\Parallax\eater_01-modified.png").convert_alpha(),
-	   pygame.image.load("assets\Parallax\eater_02-modified.png").convert_alpha(),
-	   pygame.image.load("assets\Parallax\eater_03-modified.png").convert_alpha(),
-	   pygame.image.load("assets\Parallax\eater_04-modified.png").convert_alpha(),
-	   pygame.image.load("assets\Parallax\eater_05-modified.png").convert_alpha()]
-		
+		image = []
+		for i in range(1,13):
+			image.append(pygame.image.load(f"assets\Parallax\eater_{i}-modified.png").convert_alpha())
+	elif direction == "flying":
+		image = []
+		for i in range(1,5):
+			image.append(pygame.image.load(f"assets\Parallax\\flying_bot_{i}-modified.png").convert_alpha())
+	elif direction == "slime":
+		image = []
+		for i in range(1,5):
+			image.append(pygame.image.load(f"assets\Parallax\slime_{i}-modified.png").convert_alpha())
 	return image
 	
       
@@ -88,7 +82,7 @@ class Player(pygame.sprite.Sprite):
 	def __init__(self):
 		super().__init__()
 		self.image = image("right")
-		self.rect = self.image.get_rect(midbottom=(450, SCREEN_HEIGHT - (ground_height)))
+		self.rect = self.image.get_rect(midbottom=(300, SCREEN_HEIGHT - (ground_height)))
 		self.x_velocity = 0
 		self.y_velocity = 0
 		self.on_ground = True
@@ -179,16 +173,17 @@ class Objects_to_draw(pygame.sprite.Sprite):
 
 
 class Monsters(pygame.sprite.Sprite):
-	def __init__(self,type,x_pos,y_pos):
+	def __init__(self,type,x_pos,y_pos,distance,speed):
 		super().__init__()
 		
 		self.type = image(type)
-		self.y_pos = y_pos
-		self.x_pos = x_pos             
+		self.x_pos = objects.sprites()[x_pos].rect.x           
+		self.y_pos = objects.sprites()[y_pos].rect.top
+		self.start_point = x_pos
 		self.animation_index = 0
 		self.state = False
-		self.direction = 4
-		# self.distance = random.randint(50,100)
+		self.speed = speed
+		self.distance = distance
 		self.image = self.type[int(self.animation_index)]
 		self.rect = self.image.get_rect(bottomleft = (self.x_pos,self.y_pos))
 
@@ -208,12 +203,12 @@ class Monsters(pygame.sprite.Sprite):
 		if self.animation_index > len(self.type)-1 :
 			self.animation_index = 0
 		
-		self.rect.x += self.direction
+		self.rect.x += self.speed
 		
-		for i in range(len(objects.sprites())) :
-			if self.rect.x <= objects.sprites()[i].rect.x or self.rect.x >= objects.sprites()[i].rect.x + 320:
-				self.direction *= -1  # Reverse the direction
-				self.state = not(self.state)
+		
+		if self.rect.x <= objects.sprites()[self.start_point].rect.x or self.rect.x >= objects.sprites()[self.start_point].rect.x + self.distance:
+			self.speed *= -1  # Reverse the direction
+			self.state = not(self.state)
 				
 		if self.state :
 			self.image = pygame.transform.flip(self.image, True,False)
@@ -245,6 +240,9 @@ def collide():
 				player.sprite.rect.top = objects.sprites()[i].rect.bottom 
 				player.sprite.y_velocity = 0
 				player.sprite.on_ground = True
+		for i in range(len(monsters.sprites())):
+			if monsters.sprites()[i].rect.colliderect(player.sprite.rect):
+				print('collide')
 
 	    
 
@@ -268,22 +266,34 @@ objects.add(Obstacle("big_land",2730,SCREEN_HEIGHT+500))
 objects.add(Obstacle('land',(ground_width * 10)+20, SCREEN_HEIGHT-220))
 for i in range(11,13):
 	objects.add(Obstacle('land',(ground_width * i), SCREEN_HEIGHT+20))
-for i in range(16,20):
-	objects.add(Obstacle('land',(ground_width * i), SCREEN_HEIGHT+20))
 objects.add(Obstacle('land',(ground_width * 13)+70, SCREEN_HEIGHT-150))
 objects.add(Obstacle('land',(ground_width * 15)-50, SCREEN_HEIGHT-150))
+for i in range(16,20):
+	objects.add(Obstacle('land',(ground_width * i), SCREEN_HEIGHT+20))
 
 # monsters 
 monsters = pygame.sprite.Group()
-monsters.add(Monsters('eater',objects.sprites()[2].rect.x + 1 ,objects.sprites()[2].rect.top))
-monsters.add(Monsters('eater',objects.sprites()[3].rect.x + ground_width//2 ,objects.sprites()[3].rect.top))
-monsters.add(Monsters('eater',objects.sprites()[4].rect.x + ground_width//4 ,objects.sprites()[4].rect.top))
+monsters.add(Monsters('eater',2,2,300,3))
+monsters.add(Monsters('eater',3,3,300,4))
+monsters.add(Monsters('eater',4,4,300,4))
+monsters.add(Monsters('flying',5,5,1000,2))
+monsters.add(Monsters('slime',5,5,1000,4))
+monsters.add(Monsters('eater',6,6,300,3))
+monsters.add(Monsters('flying',7,7,620,2))
+monsters.add(Monsters('slime',7,7,620,4))
+monsters.add(Monsters('slime',9,9,300,4))
+monsters.add(Monsters('eater',10,10,300,3))
+monsters.add(Monsters('flying',11,11,500,2))
+monsters.add(Monsters('flying',13,13,500,2))
+monsters.add(Monsters('slime',11,11,1300,5))
+
+
 
 
 # objects just to be drown
 objects_d = pygame.sprite.Group()
 objects_d.add(Objects_to_draw('tree',250,SCREEN_HEIGHT-ground_height+20))
-objects_d.add(Objects_to_draw('door',(ground_width * 19)+(ground_width/4),SCREEN_HEIGHT - ground_height+20))
+objects_d.add(Objects_to_draw('door',(ground_width * 19)+130,SCREEN_HEIGHT - ground_height+20))
 
 
 
@@ -301,16 +311,12 @@ while run:
 	key = pygame.key.get_pressed()
 	if key[pygame.K_LEFT] and player.sprite.rect.left < 201 and scroll > 0:
 		scroll -= 2
-		scroll_S_L = True
 	elif key[pygame.K_RIGHT] and player.sprite.rect.right > 999 and scroll < 3000:
 		scroll += 2
-		scroll_S_R = True
-	else:
-		scroll_S_R = False
-		scroll_S_L = False
+	
+	
 
-	# draw_trees() 
-	# screen.fill('Red')
+	
 	objects_d.update()  
 	objects_d.draw(screen) 
 	objects.update()  
@@ -320,7 +326,7 @@ while run:
 	player.update() 
 	player.draw(screen) 
 	collide()
-	# print(player.sprite.rect.right)
+	
 
 	#event handlers
 	for event in pygame.event.get():
